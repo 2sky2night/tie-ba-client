7adaf77336770d28e19a3d97203974721a93ea16
Kinght <1301680879@qq.com>
2023-07-19 14:23:33 +0800

    配置console.log

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import plugins from './plugins'
 
+// 生产模式下关闭浏览器log调试
+if (import.meta.env.MODE !== 'development') {
+  console.log=()=>{}
+}
 
 const app = createApp(App)
 app.use(plugins)
